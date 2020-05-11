8d4baa66b52dfd9af5050e686e4fa26a2c7a61e7
odidev <odidev@puresoftware.com>
2020-05-11 14:44:39 +0530

    Update BrowserFetcher.ts

--- src/BrowserFetcher.ts
+++ src/BrowserFetcher.ts
@@ -223,7 +223,7 @@ export class BrowserFetcher {
       if (os.arch() === 'arm64') {
         fs.stat('/usr/bin/chromium-browser', function (err, stats) {
           if (stats === undefined) {
-            console.error(` .Chromium Binary is not available for aarch64, Download it manually. \n `);
+            console.error(` Chromium Binary is not available for aarch64, Download it manually.\n `);
             console.error(` If you are on Ubuntu,You can install with: `);
             console.error(` \n apt-get install chromium-browser\n`);
             throw new Error();
@@ -239,7 +239,8 @@ export class BrowserFetcher {
     }
     const revisionInfo = this.revisionInfo(revision);
     if (revisionInfo) {
-      if (os.arch() !== 'arm64') await chmodAsync(revisionInfo.executablePath, 0o755);
+      if (os.arch() !== 'arm64') 
+        await chmodAsync(revisionInfo.executablePath, 0o755);
       return revisionInfo;
     }
   }
